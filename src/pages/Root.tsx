deba988fe653a0722b50f1010c771a520356d8db
Michael Smith <mliiha5ahiilm@gmail.com>
2026-06-08 22:12:24 +0300

    исравление отображения

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router'
 
 export const Root = () => (
-    <div className="flex flex-col h-screen overflow-hidden">
-        <div className="flex-1 overflow-hidden">
-            <Outlet />
-        </div>
+    <div className="flex flex-col h-dvh">
+        <Outlet />
         <footer className="py-2 text-center text-sm text-slate-400 font-golos shrink-0">
             @mikesmth {new Date().getFullYear()}
         </footer>
